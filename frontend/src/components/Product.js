8f5fd2861912028bffbeaa552ceab61b62d63256
Anmol-7 <jainanmol5528@gmail.com>
2020-10-14 17:20:19 +0530

    redux for productlist in homepage

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,7 +6,7 @@ import NumberFormat from "react-number-format";
 
 const Product = ({ product }) => {
   return (
-    <Card className="my-3 p-3 rounded h-100 mb-3">
+    <Card className="my-3 p-3 rounded ">
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
       </Link>
