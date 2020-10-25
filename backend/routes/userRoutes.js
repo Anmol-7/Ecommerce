a82b0b9e509e386e632ae38463baf8bd7f46287f
Anmol-7 <jainanmol5528@gmail.com>
2020-10-25 14:12:28 +0530

    backend user authentication

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+import {protect} from '../middleware/authMiddleware.js'
+import { authUser,getUserProfile } from "../controllers/userController.js";
+
+router.post('/login',authUser)
+router.route('/profile').get(protect,getUserProfile)
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,27 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import colors from 'colors';
-import connectDB from './config/db.js';
-import {notFound,errorHandler} from './middleware/errorMiddleware.js'
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app=express();
+const app = express();
 
-app.use('/api/products',productRoutes);
+app.use(express.json());
 
-app.use(notFound)
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
-app.use(errorHandler)
+app.use(notFound);
 
-const PORT=process.env.PORT||5000
+app.use(errorHandler);
 
-app.listen(PORT,console.log('server running'.yellow.bold));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log("server running".yellow.bold));
